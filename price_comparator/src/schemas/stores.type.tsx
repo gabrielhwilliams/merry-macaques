21a1c692738c4c88ed8746f492b89a833570d906
Gibril <gabrielhwgo1@gmail.com>
2026-02-28 16:56:24 -0500

    technically, it works

---
+++ price_comparator/src/schemas/stores.type.tsx
@@ -0,0 +1,14 @@
+export type Stores = {
+    stores: [
+        {
+            name: string,
+            items: [
+                {
+                    ingredientId: string,
+                    unit_of_measure: string,
+                    quantity: number,
+                    price: number | null,
+                }
+            ]
+        }
+    ]}

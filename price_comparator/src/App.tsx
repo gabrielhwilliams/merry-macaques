3cdea79bd3249d68236bdbc2ab13ccecd9dbde14
mot7506 <mot7506@g.rit.edu>
2026-02-28 14:36:28 -0500

    html structure

--- price_comparator/src/App.tsx
+++ price_comparator/src/App.tsx
@@ -8,26 +8,40 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="Top">
+        <div className="Location">
+          <div className="LocationStringInput">
+
+          </div>
+          <div className="StoreSelect">
+
+          </div>
+        </div>
+        <div className="Share">
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="Middle">
+        <div className="ShoppingList">
+
+        </div>
+        <div className="RescipeList">
+
+        </div>
+        <div className="PricesList">
+
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="Bottom">
+        <div className="Chat">
+          <div className="ChatInput">
+
+          </div>
+          <div className="ChatSend">
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
